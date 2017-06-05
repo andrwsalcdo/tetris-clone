b879f5865b94913327aaca770aa9cd4fde7645c6
Andrew Salcedo <asalcedo07@gmail.com>
2017-06-04 20:28:21 -0400

    refactor: player down movment into playerDrop()

--- tetris.js
+++ tetris.js
@@ -28,6 +28,12 @@ function drawMatrix(matrix, offset) {
         });
     }); 
 }
+
+function playerDrop() {
+    player.pos.y++;
+    dropCounter = 0; // u want a drop delay after press down. 
+}
+
 let dropCounter = 0; 
 let dropInterval = 1000; // drop pieces every 1 sec 
 
@@ -38,8 +44,7 @@ function update(time = 0) {
 
     dropCounter += deltaTime; 
     if (dropCounter > dropInterval) {
-        player.pos.y++; 
-        dropCounter = 0; 
+        playerDrop(); 
     } // this is the drop piece movment 
 
     draw(); //moves the pieces 
@@ -52,14 +57,12 @@ const player = {
 }
 
 document.addEventListener('keydown', event => {
-    // console.log(event);
     if ( event.keyCode === 37) {
         player.pos.x--; 
     } else if (event.keyCode === 39) {
         player.pos.x++; 
     } else if (event.keyCode === 40) {
-        player.pos.y++;
-        dropCounter = 0;  
+        playerDrop(); 
     }
     
 }); 
